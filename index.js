338c330cbe011b82e6f4ed139fd1ccd5e9998c7c
nurulalam <jack245200@gmail.com>
2022-11-26 02:37:28 +0600

    products category show from database

--- index.js
+++ index.js
@@ -2,13 +2,43 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 //mideleware
 
 app.use(cors());
 app.use(express.json());
 
+//oparation
 app.get("/", async (req, res) => {
   res.send("SwapMart running on");
 });
+
+//database oparation
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3kkv8ke.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productsCategoryCollection = client
+      .db("swapMart")
+      .collection("productsCategory");
+
+    //products category load
+    app.get("/productsCategory", async (req, res) => {
+      const query = {};
+      const result = await productsCategoryCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
 app.listen(port, console.log("Port running on", port));
