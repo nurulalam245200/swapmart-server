9b3a6a0f40747f2d3c44b463a83af2a60c6fa86c
nurulalam <jack245200@gmail.com>
2022-11-27 22:00:04 +0600

    order cart and verify jwt token

--- index.js
+++ index.js
@@ -22,7 +22,22 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+//JWT TOKEN function
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("Unauthorized Access");
+  }
+  const token = authHeader.split(" ")[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+    if (error) {
+      return res.status(403).send(message, "Forbiden Accesss");
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
   try {
     const productsCategoryCollection = client
@@ -31,6 +46,23 @@ async function run() {
     const productsCollection = client.db("swapMart").collection("products");
     const sellersCollection = client.db("swapMart").collection("sellers");
     const usersCollection = client.db("swapMart").collection("users");
+    const ordersCollection = client.db("swapMart").collection("orders");
+
+    //jwt
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
+
     //products category load
     app.get("/productsCategory", async (req, res) => {
       const query = {};
@@ -38,7 +70,7 @@ async function run() {
       res.send(result);
     });
 
-    //get products
+    //get categoryproducts
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { category_Id: id };
@@ -52,6 +84,14 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //cartorder
+    //add to cart
+    app.post("/cart", verifyJWT, async (req, res) => {
+      const cart = req.body;
+      const result = await ordersCollection.insertOne(cart);
       res.send(result);
     });
   } finally {
